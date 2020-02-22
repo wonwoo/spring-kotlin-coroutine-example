19de7ce80f5f995464ab1ff1bc59c1481cf7027b
wonwoo <aoruqjfu@gmail.com>
2020-02-22 19:49:32 +0900

    polish

--- src/main/kotlin/ml/wonwoo/springkotlincoroutineexample/product/HistoryRepository.kt
+++ src/main/kotlin/ml/wonwoo/springkotlincoroutineexample/product/HistoryRepository.kt
@@ -1,10 +1,8 @@
 package ml.wonwoo.springkotlincoroutineexample.product
 
 import kotlinx.coroutines.flow.Flow
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.data.mongodb.repository.Tailable
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import reactor.core.publisher.Flux
 
 interface HistoryRepository : CoroutineCrudRepository<History, String> {
 
